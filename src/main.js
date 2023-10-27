437d5095208842874c5176ede9bdc13df13ffbe4
tweibs <145374480+tweibs@users.noreply.github.com>
2023-10-27 14:45:39 -0600

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
+
 
 /**
  * The main function for the action.
@@ -7,20 +8,25 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const TOKEN = core.getInput('token')
+    const OWNER = github.context.repo.owner
+    const REPO = github.context.repo
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = new Octokit({
+      auth: TOKEN
+    })
+    
+    const response = await octokit.request('GET /repos/${owner}/{repo}/dependabot/alerts', {
+      owner: OWNER,
+      repo: REPO,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.setOutput('dependabot-alert-count', response.data.length)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
 }
