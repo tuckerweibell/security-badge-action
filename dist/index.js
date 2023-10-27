316aa1b2933fe2f1b70bb91d84c7d418b38d0ede
tweibs <145374480+tweibs@users.noreply.github.com>
2023-10-27 14:48:53 -0600

    npm run all

--- dist/index.js
+++ dist/index.js
@@ -29864,7 +29864,6 @@ function wrappy (fn, cb) {
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 
-
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -29875,20 +29874,20 @@ async function run() {
     const OWNER = github.context.repo.owner
     const REPO = github.context.repo
 
-    const octokit = new Octokit({
-      auth: TOKEN
-    })
-    
-    const response = await octokit.request('GET /repos/${owner}/{repo}/dependabot/alerts', {
-      owner: OWNER,
-      repo: REPO,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+    const octokit = github.getOctokit(TOKEN)
+
+    const response = await octokit.request(
+      'GET /repos/${owner}/{repo}/dependabot/alerts',
+      {
+        owner: OWNER,
+        repo: REPO,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
       }
-    })
+    )
 
     core.setOutput('dependabot-alert-count', response.data.length)
-
   } catch (error) {
     core.setFailed(error.message)
   }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -12,20 +11,20 @@ async function run() {
     const OWNER = github.context.repo.owner
     const REPO = github.context.repo
 
-    const octokit = new Octokit({
-      auth: TOKEN
-    })
-    
-    const response = await octokit.request('GET /repos/${owner}/{repo}/dependabot/alerts', {
-      owner: OWNER,
-      repo: REPO,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+    const octokit = github.getOctokit(TOKEN)
+
+    const response = await octokit.request(
+      'GET /repos/${owner}/{repo}/dependabot/alerts',
+      {
+        owner: OWNER,
+        repo: REPO,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
       }
-    })
+    )
 
     core.setOutput('dependabot-alert-count', response.data.length)
-
   } catch (error) {
     core.setFailed(error.message)
   }
