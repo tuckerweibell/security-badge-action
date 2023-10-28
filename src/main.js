19563fe6c042191911cc5ad624e85404dd87bf56
tweibs <145374480+tweibs@users.noreply.github.com>
2023-10-28 09:25:01 -0600

    fix issues

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const chroma = require('chroma-js')
  */
 async function run() {
   try {
-    const color_scale = chroma.scale(['green', 'yellow', 'red'])
+    const color_scale = chroma.scale(['#00e604','yellow', 'red'])
     const token = core.getInput('token')
     const dependabotMinimum = core.getInput('dependabot-minimum')
     const dependabotMaximum = core.getInput('dependabot-maximum')
